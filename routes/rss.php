8d39537a37d396d3c9cebbd5ba04ad6cffc3383c
Roardom <roardom@protonmail.com>
2025-06-09 03:02:59 +0000

    refactor: remove unused `before` route filter middleware
    Laravel deprecated route filters in version 5.0, and completely removed support for them in 5.2. This `before` might as well not exist. The routes are protected within the controller actions themselves currently.

--- routes/rss.php
+++ routes/rss.php
@@ -25,8 +25,6 @@ if (config('unit3d.root_url_override')) {
     URL::forceRootUrl(config('unit3d.root_url_override'));
 }
 
-Route::group(['before' => 'auth'], function (): void {
-    // RSS (RSS Key Auth)
-    Route::get('/rss/{id}.{rsskey}', [App\Http\Controllers\RssController::class, 'show'])->name('rss.show.rsskey');
-    Route::get('/torrent/download/{id}.{rsskey}', [App\Http\Controllers\TorrentDownloadController::class, 'store'])->name('torrent.download.rsskey');
-});
+// RSS (RSS Key Auth)
+Route::get('/rss/{id}.{rsskey}', [App\Http\Controllers\RssController::class, 'show'])->name('rss.show.rsskey');
+Route::get('/torrent/download/{id}.{rsskey}', [App\Http\Controllers\TorrentDownloadController::class, 'store'])->name('torrent.download.rsskey');
